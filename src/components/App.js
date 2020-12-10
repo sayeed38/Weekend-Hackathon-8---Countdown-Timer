46c960c8481c1e8dbce7b35df14b692e1c6e4f53
Mohammed Sayeed <34248417+sayeed38@users.noreply.github.com>
2020-12-11 00:36:41 +0530

    Added Timer Feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,51 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+  // write your code here
+  const [input, setInput] = useState("");
+  const [timer, setTimer] = useState(0);
 
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (timer <= 0) {
+        return;
+      }
+      setTimer((timer) => timer - 1);
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    if (
+      (e.keyCode >= 48 && e.keyCode <= 57) ||
+      (e.keyCode >= 96 && e.keyCode <= 105)
+    ) {
+      setInput(input + e.key);
+    }
+    if (e.keyCode === 13) {
+      setTimer(parseInt(input, 10));
+      setInput("");
+    }
+  };
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input
+            id="timeCount"
+            value={input}
+            onKeyDown={(event) => handleInput(event)}
+          />{" "}
+          sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timer}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
